c054952840c33ac0aa56c7a116af76c2d9270113
mhpk <116509480+mhpk@users.noreply.github.com>
2022-10-23 19:05:03 +0200

    Update easyAimbot.lua

--- easyAimbot.lua
+++ easyAimbot.lua
@@ -2,4 +2,3 @@ local mychar = game:GetService("Players").LocalPlayer.Character
 local cam = game:GetService("Workspace").Camera
 
 cam.CFrame =  CFrame.new(mychar.HumanoidRootPart.Position,placetoaim.Position+Vector3.new(0,0,0))
-1
