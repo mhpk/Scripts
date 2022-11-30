2b72eac1930aa967b45e31d531d97c27ecd1fff1
mhpk <mhpk@proton.me>
2022-11-30 03:52:03 +0200

    Update camlock.lua

--- camlock.lua
+++ camlock.lua
@@ -2,3 +2,5 @@ local myChar = game:GetService("Players").LocalPlayer.Character
 local Cam = game:GetService("Workspace").Camera
 
 Cam.CFrame =  CFrame.new(myChar.HumanoidRootPart.Position,placetoaim.Position+Vector3.new(0,0,0))
+
+--// shitty camlock script i made, works in games like hoopz
